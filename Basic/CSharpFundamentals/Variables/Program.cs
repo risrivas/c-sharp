710409757cf5ac561f4441bee5e7bd16952cbf46
Rishi Srivastava <rishisrivasta@gmail.com>
2018-07-21 09:03:50 +0800

    added Variables project

---
+++ Basic/CSharpFundamentals/Variables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14159f;
+            Console.WriteLine(Pi);
+
+        }
+    }
+}
