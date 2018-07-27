ff2848e6ef7f39b8b29cf486f3569ab2044a2d0d
Rishi Srivastava <rishisrivasta@gmail.com>
2018-07-27 19:59:06 +0800

    added Operators class

---
+++ Basic/CSharpFundamentals/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine((float)a / b);
+
+            Console.WriteLine(1 + 2 * 3);
+
+            Console.WriteLine(1 > 2);
+        }
+    }
+}
