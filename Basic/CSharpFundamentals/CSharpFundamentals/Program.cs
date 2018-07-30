c7d745f3eeada3566a0f98e1431290a953f99122
Rishi Srivastava <rishisrivasta@gmail.com>
2018-07-31 06:34:04 +0800

    added arrays demo

--- Basic/CSharpFundamentals/CSharpFundamentals/Program.cs
+++ Basic/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -20,6 +20,19 @@ namespace CSharpFundamentals
             var calculator = new Calculator();
             Console.WriteLine(calculator.Add(3, 6));
 
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Harry", "Mick", "Lucas" };
 
 
             /*var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
