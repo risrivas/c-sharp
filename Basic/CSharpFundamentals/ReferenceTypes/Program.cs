c533ee8e08db1cb211f8a408df8e5bd7b126c9b7
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-07 06:51:58 +0800

    reference types exercise

---
+++ Basic/CSharpFundamentals/ReferenceTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine($"a: {a} b: {b}");
+
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[1] = 0;
+            Console.WriteLine($"array1[1]: {array1[1]} array2[1]: {array2[1]}");
+        }
+    }
+}
