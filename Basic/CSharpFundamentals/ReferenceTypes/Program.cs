699ff7998d40eb0f964a0219544c3dae48beb1c6
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-10 06:17:04 +0800

    resharper shortcuts and reference types

--- Basic/CSharpFundamentals/ReferenceTypes/Program.cs
+++ Basic/CSharpFundamentals/ReferenceTypes/Program.cs
@@ -15,6 +15,33 @@ namespace ReferenceTypes
             var array2 = array1;
             array2[1] = 0;
             Console.WriteLine($"array1[1]: {array1[1]} array2[1]: {array2[1]}");
+
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person()
+            {
+                Age = 10
+            };
+
+            MakeOld(person);
+            Console.WriteLine(person.Age);
         }
+
+        private static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+
+        private static void Increment(int number)
+        {
+            number += 10;
+        }
+    }
+
+    class Person
+    {
+        public int Age;
     }
 }
