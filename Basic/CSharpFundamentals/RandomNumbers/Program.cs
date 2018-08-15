0c80c93c3a91e7de142146177e47673bbf5e099a
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-16 04:50:18 +0800

    random numbers

---
+++ Basic/CSharpFundamentals/RandomNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
