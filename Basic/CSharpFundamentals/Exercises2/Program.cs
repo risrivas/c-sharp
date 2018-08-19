f47fc1b69da20d3d69a4a8c189d79e55d899471a
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-19 08:02:59 +0800

    completed exercises on loops

--- Basic/CSharpFundamentals/Exercises2/Program.cs
+++ Basic/CSharpFundamentals/Exercises2/Program.cs
@@ -6,7 +6,59 @@ namespace Exercises2
     {
         static void Main(string[] args)
         {
-            Exercise2_2();
+            Exercise2_5();
+        }
+
+        static void Exercise2_5()
+        {
+            Console.Write("enter a series of numbers separated by comma: ");
+            var input = Console.ReadLine();
+
+            string[] numbers = input.Split(',');
+            int max = int.MinValue;
+            foreach (string number in numbers)
+            {
+                //int num = Convert.ToInt32(number);
+                //if (num > max)
+                //{
+                //    max = num;
+                //}
+                max = Math.Max(max, Convert.ToInt32(number));
+            }
+            Console.WriteLine(max);
+        }
+
+        static void Exercise2_4()
+        {
+            var secretNumber = new Random().Next(1, 10);
+
+            for (var i = 1; i <= 4; i++)
+            {
+                Console.Write("pick a random number between 1 and 10: ");
+                var input = Convert.ToInt32(Console.ReadLine());
+                if (input == secretNumber)
+                {
+                    Console.WriteLine("You won");
+                    return;
+                }
+            }
+
+            Console.WriteLine("You lose. Secret number is: " + secretNumber);
+
+        }
+
+        static void Exercise2_3()
+        {
+            int fact = 1;
+            Console.Write("enter a number: ");
+            var input = Convert.ToInt32(Console.ReadLine());
+            int number = input;
+            while (input >= 1)
+            {
+                fact *= input--;
+            }
+            Console.WriteLine($"{number}! = {fact}");
+
         }
 
         static void Exercise2_2()
