9a09a357cf00bcc77fa77a54d6080a3a426bb5f1
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-29 05:38:29 +0800

    List demo

---
+++ Basic/CSharpFundamentals/ListDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+            // numbers.Remove(1);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
+
+        }
+    }
+}
