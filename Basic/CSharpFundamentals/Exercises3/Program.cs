9d0e2c119858739077184f95ee777c92a2ced701
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-30 05:45:50 +0800

    exercises3

---
+++ Basic/CSharpFundamentals/Exercises3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise3_1();
+        }
+
+        static void Exercise3_1()
+        {
+            var namesList = new List<string>();
+            while (true)
+            {
+                Console.Write("enter name: ");
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                namesList.Add(input.Trim());
+            }
+
+            var size = namesList.Count;
+            if (size == 0) return;
+            if (size == 1) Console.WriteLine("[{0}] likes your post.", namesList[0]);
+            if (size == 2) Console.WriteLine("[{0}] and [{1}] like your post.", namesList[0], namesList[1]);
+            else Console.WriteLine("[{0}], [{1}] and [{2}] others like your post.", namesList[0], namesList[1], size - 2);
+        }
+    }
+}
