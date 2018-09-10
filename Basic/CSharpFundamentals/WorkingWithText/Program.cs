7f46d7be7f383efaca3293375741bcfb3ed4850a
Rishi Srivastava <rishisrivasta@gmail.com>
2018-09-11 05:42:21 +0800

    Working with Text

---
+++ Basic/CSharpFundamentals/WorkingWithText/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Rishi Srivastava  ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Rishi", "Moshfegh"));
+
+            if (string.IsNullOrEmpty(null)
+                && string.IsNullOrEmpty("")
+                && string.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C"));
+            Console.WriteLine(price.ToString("C0"));
+        }
+    }
+}
