a19d40d63ff959c65e4bada00e75ada5f5baa083
Rishi Srivastava <rishisrivasta@gmail.com>
2018-09-13 06:19:37 +0800

    Summarising Text

---
+++ Basic/CSharpFundamentals/SummarisingText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SummarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really really long text.";
+            Console.WriteLine(StringUtility.SummarizeText(sentence, 25));
+        }
+
+
+    }
+
+
+}
---
+++ Basic/CSharpFundamentals/SummarisingText/StringUtility.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace SummarisingText
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string sentence, int maxLength = 20)
+        {
+            if (sentence.Length < maxLength)
+            {
+                return sentence;
+            }
+
+            // sentence.Substring(0, maxLength);
+            var words = sentence.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength) break;
+            }
+
+            return string.Join(" ", summaryWords) + "...";
+        }
+    }
+}
