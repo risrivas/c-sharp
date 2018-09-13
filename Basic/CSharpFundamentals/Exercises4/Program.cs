0c449bf0dd784b43c1dc5e8665152565a3218675
Rishi Srivastava <rishisrivasta@gmail.com>
2018-09-14 06:32:30 +0800

    StringBuilder

---
+++ Basic/CSharpFundamentals/Exercises4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercises4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise4_1();
+        }
+
+        static void Exercise4_1()
+        {
+            Console.Write("enter few numbers separated by hyphen: ");
+            var input = Console.ReadLine();
+            var numbers = input.Split('-');
+            var first = Convert.ToInt32(numbers[0]);
+
+            var isConsecutive = true;
+
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                var number = Convert.ToInt32(numbers[i]);
+                if ((number != (first + 1)) || (number != (first - 1)))
+                {
+                    isConsecutive = false;
+                    break;
+                }
+                first = number;
+            }
+
+            if (isConsecutive)
+            {
+                Console.WriteLine("Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Not Consecutive");
+            }
+        }
+    }
+}
---
+++ Basic/CSharpFundamentals/StringBuilderDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '*')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char: " + builder[0]);
+        }
+    }
+}
