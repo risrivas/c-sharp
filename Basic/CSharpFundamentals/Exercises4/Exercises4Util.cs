cf4b50b4f50d639d799b9dd8c00ad21b42121e11
Rishi Srivastava <rishisrivasta@gmail.com>
2018-09-16 08:40:46 +0800

    Exercises4_2

---
+++ Basic/CSharpFundamentals/Exercises4/Exercises4Util.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises4
+{
+    public class Exercises4Util
+    {
+        public static bool IsConsecutive(string[] numbers)
+        {
+            var first = Convert.ToInt32(numbers[0]);
+
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                var number = Convert.ToInt32(numbers[i]);
+                if (!((number == (first + 1)) || (number == (first - 1))))
+                {
+                    return false;
+                }
+                first = number;
+            }
+
+            return true;
+        }
+
+        public static bool IsDuplicate(string[] numbers)
+        {
+            var set = new HashSet<string>();
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var canAdd = set.Add(numbers[i]);
+                if (!canAdd)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- Basic/CSharpFundamentals/Exercises4/Program.cs
+++ Basic/CSharpFundamentals/Exercises4/Program.cs
@@ -6,7 +6,24 @@ namespace Exercises4
     {
         static void Main(string[] args)
         {
-            Exercise4_1();
+            // Exercise4_1();
+            Exercise4_2();
+        }
+
+        static void Exercise4_2()
+        {
+            Console.Write("enter few numbers separated by hyphen: ");
+            var input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                var numbers = input.Split('-');
+
+                if (Exercises4Util.IsDuplicate(numbers))
+                {
+                    Console.WriteLine("Duplicate");
+                }
+            }
+
         }
 
         static void Exercise4_1()
@@ -14,22 +31,8 @@ namespace Exercises4
             Console.Write("enter few numbers separated by hyphen: ");
             var input = Console.ReadLine();
             var numbers = input.Split('-');
-            var first = Convert.ToInt32(numbers[0]);
 
-            var isConsecutive = true;
-
-            for (var i = 1; i < numbers.Length; i++)
-            {
-                var number = Convert.ToInt32(numbers[i]);
-                if ((number != (first + 1)) || (number != (first - 1)))
-                {
-                    isConsecutive = false;
-                    break;
-                }
-                first = number;
-            }
-
-            if (isConsecutive)
+            if (Exercises4Util.IsConsecutive(numbers))
             {
                 Console.WriteLine("Consecutive");
             }
