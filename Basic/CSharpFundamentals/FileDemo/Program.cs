aba6f4d0ba8f992ac4f210db72d3958f20d01906
Rishi Srivastava <rishisrivasta@gmail.com>
2018-09-23 19:06:20 +0800

    File and FileInfo

---
+++ Basic/CSharpFundamentals/FileDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\somefile.jpg";
+
+            File.Copy(@"C:\Users\rishi\Desktop\fb\IMG_0615.jpg", @"C:\Users\rishi\Desktop\IMG_0615.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                // 
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+            //fileInfo.IsReadOnly()
+        }
+    }
+}
