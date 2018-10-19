c20a24bfb509afcde6174314d20bb237283ee99a
Rishi Srivastava <rishisrivasta@gmail.com>
2018-10-20 05:52:43 +0800

    access modifiers

---
+++ Basic/CSharpFundamentals/AccessModifiers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthDate;
+
+        public void SetBirthDate(DateTime birthDate)
+        {
+            _birthDate = birthDate;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return _birthDate;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthDate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthDate());
+        }
+    }
+}
