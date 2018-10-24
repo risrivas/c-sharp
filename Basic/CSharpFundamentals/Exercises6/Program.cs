18c4a928afc9543e5ede1c86ce0f3a17fad4e38e
Rishi Srivastava <rishisrivasta@gmail.com>
2018-10-25 07:05:59 +0800

    intermediate exercises6

---
+++ Basic/CSharpFundamentals/Exercises6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercises6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercises6_1();
+        }
+
+        static void Exercises6_1()
+        {
+            var stopWatch = new StopWatch();
+
+            stopWatch.Start();
+            Thread.Sleep(2000);
+            stopWatch.Stop();
+
+            Console.WriteLine(stopWatch.Duration().Seconds);
+        }
+    }
+}
---
+++ Basic/CSharpFundamentals/Exercises6/StopWatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercises6
+{
+    public class StopWatch
+    {
+        private DateTime _start = DateTime.Now;
+        private DateTime _stop = DateTime.Now;
+        private bool _isStarted = false;
+
+        public void Start()
+        {
+            if (!_isStarted)
+            {
+                _start = DateTime.Now;
+                _isStarted = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is already started");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_isStarted)
+            {
+                _stop = DateTime.Now;
+                _isStarted = false;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is NOT started");
+            }
+
+        }
+
+        public TimeSpan Duration()
+        {
+            return _stop - _start;
+        }
+    }
+}
