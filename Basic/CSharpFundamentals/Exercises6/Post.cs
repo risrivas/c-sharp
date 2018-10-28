d65d0f49a957596d34b459dcde70af825be17904
Rishi Srivastava <rishisrivasta@gmail.com>
2018-10-29 05:48:35 +0800

    Composition and Inheritance

---
+++ Basic/CSharpFundamentals/Exercises6/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercises6
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        private int Votes = 0;
+
+        public void UpVote()
+        {
+            Votes++;
+        }
+
+        public void DownVote()
+        {
+            if (Votes > 0)
+            {
+                Votes--;
+            }
+        }
+
+        public int GetVotes()
+        {
+            return Votes;
+        }
+    }
+}
--- Basic/CSharpFundamentals/Exercises6/Program.cs
+++ Basic/CSharpFundamentals/Exercises6/Program.cs
@@ -7,7 +7,27 @@ namespace Exercises6
     {
         static void Main(string[] args)
         {
-            Exercises6_1();
+            // Exercises6_1();
+            Exercises6_2();
+        }
+
+        static void Exercises6_2()
+        {
+            var post = new Post()
+            {
+                Title = "StackOverflow Post",
+                Description = "This post is similar to StackOverflow post",
+                CreateDate = DateTime.Now
+            };
+
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+
+            post.DownVote();
+
+            Console.WriteLine("Votes: {0}", post.GetVotes());
         }
 
         static void Exercises6_1()
@@ -15,8 +35,12 @@ namespace Exercises6
             var stopWatch = new StopWatch();
 
             stopWatch.Start();
+            // stopWatch.Start();
+
             Thread.Sleep(2000);
+
             stopWatch.Stop();
+            // stopWatch.Stop();
 
             Console.WriteLine(stopWatch.Duration().Seconds);
         }
