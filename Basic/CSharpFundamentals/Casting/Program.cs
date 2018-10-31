97eb7c434c1d29372f89b23e6a92a28f5a69029d
Rishi Srivastava <rishisrivasta@gmail.com>
2018-10-31 15:40:28 +0800

    Casting and Boxing

---
+++ Basic/CSharpFundamentals/Casting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //StreamReader reader = new StreamReader(new MemoryStream());
+
+            Shape shape = new Text();
+            Text text = (Text)shape;
+
+
+            //var list = new ArrayList();
+            //list.Add(1);
+            //list.Add("Mosh");
+            //list.Add(new Text());
+
+            //var anotherList = new List<Shape>();
+
+            //Text text = new Text();
+            //Shape shape = text;
+
+            //text.Width = 200;
+            //shape.Width = 100;
+            //Console.WriteLine(text.Width);
+        }
+    }
+}
---
+++ Basic/CSharpFundamentals/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            if (button != null)
+            {
+                MessageBox.Show(button.ActualHeight.ToString());
+            }
+
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
