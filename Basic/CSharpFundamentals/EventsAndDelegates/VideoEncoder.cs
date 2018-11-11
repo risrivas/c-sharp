0535b397e08354059445197efb85f1bb71663f5d
Rishi Srivastava <rishisrivasta@gmail.com>
2018-11-12 05:57:42 +0800

    Events and Delegates

---
+++ Basic/CSharpFundamentals/EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // 1. Define a delegate
+        // 2. Define an event based on that delegate
+        // 3. Raise the event
+
+        // public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        // EventHandler<TEventArgs>
+
+        // public event VideoEncodedEventHandler VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+            }
+        }
+    }
+}
