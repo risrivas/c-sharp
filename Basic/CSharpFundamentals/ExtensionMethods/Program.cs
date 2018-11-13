f0347c90541dac4a31fc2c8d580f8bee21a85a79
Rishi Srivastava <rishisrivasta@gmail.com>
2018-11-14 06:52:46 +0800

    Extension methods

---
+++ Basic/CSharpFundamentals/ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah....";
+            Console.WriteLine(post.Shorten(5));
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            Console.WriteLine(numbers.Max());
+        }
+    }
+}
